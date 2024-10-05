4c6a91c62f06a6c52bfa5703927a10b7a961ff0e
Mishnah7 <67045896+Mishnah7@users.noreply.github.com>
2024-10-05 17:57:21 +0300

    Update Dashboard.java

--- addressbook/src/com/addressbook/UI/Dashboard.java
+++ addressbook/src/com/addressbook/UI/Dashboard.java
@@ -94,17 +94,11 @@ abstract class AbstractDashboard extends JFrame {
     // Now showAboutDialog is directly defined in the abstract class and used
     public void showAboutDialog() {
         String[] links = {
-                "https://t.me/dagmawy",
-                "https://t.me/blen96",
-                "https://t.me/Ruth08",
-                "https://t.me/Hermi15"
+                "https://t.me/realloc"
         };
 
         String[] developerNames = {
-                "Dagmawi Asfaw",
-                "Blen Belay",
-                "Ruth Berhanu",
-                "Hermela Abebayehu"
+                "Dagmawi Asfaw"
         };
 
         JTextPane textPane = createAboutTextPane(links, developerNames);
